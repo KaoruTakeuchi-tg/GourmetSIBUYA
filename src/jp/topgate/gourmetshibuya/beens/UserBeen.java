29a2f5d886a1f45e30cdbfc385e81b7ff8b5c808
KentaIzumida-tg <kenta.izumida@topgate.co.jp>
2020-05-21 15:03:17 +0900

    index.jspにログインを追加

--- src/jp/topgate/gourmetshibuya/beens/UserBeen.java
+++ src/jp/topgate/gourmetshibuya/beens/UserBeen.java
@@ -1,5 +1,26 @@
 package jp.topgate.gourmetshibuya.beens;
 
 public class UserBeen {
-
+	
+	private String id;
+	private String password;
+	public UserBeen(String id, String password){};
+	
+	public String gerId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String gerPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
+
+
--- src/jp/topgate/gourmetshibuya/servlet/Login.java
+++ src/jp/topgate/gourmetshibuya/servlet/Login.java
@@ -1,11 +1,15 @@
 package jp.topgate.gourmetshibuya.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jp.topgate.gourmetshibuya.beens.UserBeen;
 
 /**
  * Servlet implementation class Login
@@ -14,28 +18,28 @@ import javax.servlet.http.HttpServletResponse;
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		//ログイン処理
+		//LoginDao loguindao = new LoginDao();
+		//boolean isLogin = logindao.execute(id, password);
+		
+		//ログイン成功時の処理
+		if (isLogin) {
+			UserBeen userbeen = new UserBeen(id, password);
+			//ユーザ情報をセッションスコープに保存
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", id);
+		}
+		
+		//トップページにフォワード
+		//RequestDispatchar dispatcher = request.getRequestDispatcher("");
+		//dispatcher.forward(request, response);
 	}
 
 }
