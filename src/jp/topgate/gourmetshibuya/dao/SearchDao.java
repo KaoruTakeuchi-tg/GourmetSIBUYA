9f4185df4f1bc7b4e5f0704951592cb884721453
KentaIzumida-tg <kenta.izumida@topgate.co.jp>
2020-05-27 16:54:07 +0900

    setStringエラー

--- src/jp/topgate/gourmetshibuya/dao/SearchDao.java
+++ src/jp/topgate/gourmetshibuya/dao/SearchDao.java
@@ -14,41 +14,39 @@ public class SearchDao {
 
 
 	public RestaurantList Search(String text) {
-		RestaurantBean rb = new RestaurantBean();
 		String url = "jdbc:mysql://127.0.0.1:13306/main?serverTimezone=UTC";
 		String user = "root";
 		String password = "kaoru2106";
+		String input = text;
 		int count;
 		RestaurantList searchResult = new RestaurantList();
 		try {
 			Connection con = DriverManager.getConnection(url, user, password);
 			System.out.println("Connected....");
 			try (Statement st = con.createStatement()){
-				String sql2 = "SELECT * FROM main.restaurans_categories WHERE CONCAT(name, place, phonenum, genres) LIKE \"%%%\";";
+				String sql2 = "SELECT * FROM main.restaurans_categories WHERE CONCAT(name, place, phonenum, genres) LIKE \"%?%\";";
 				PreparedStatement pStmt2 = con.prepareStatement(sql2);
-				pStmt2.setString(2, text);
-				try (ResultSet rs = pStmt2.executeQuery()) {
-					while(rs.next()) {
-						rb.setBusiness_hours(rs.getString("business_hours"));
-						rb.setContent(rs.getString("content"));
-						rb.setEvaluation(rs.getInt("evaluation"));
-						rb.setGenres(rs.getString("genres"));
-						rb.setName(rs.getString("name"));
-						rb.setPhonenum(rs.getString("phonenum"));
-						rb.setPhoto(rs.getString("photo"));
-						rb.setPlace(rs.getString("place"));
-						rb.setRegular_holiday(rs.getString("regular_holiday"));
-						rb.setRestaurant_id(rs.getInt("restaurant_id"));
-						rb.setUrl(rs.getString("url"));
-						searchResult.addResutaurantList(rb);
-					}
-
+				pStmt2.setString(1, input);
+				ResultSet rs = pStmt2.executeQuery();
+				while(rs.next()) {
+					RestaurantBean rb = new RestaurantBean();
+					rb.setBusiness_hours(rs.getString("business_hours"));
+					rb.setContent(rs.getString("content"));
+					rb.setEvaluation(rs.getInt("evaluation"));
+					rb.setGenres(rs.getString("genres"));
+					rb.setName(rs.getString("name"));
+					rb.setPhonenum(rs.getString("phonenum"));
+					rb.setPhoto(rs.getString("photo"));
+					rb.setPlace(rs.getString("place"));
+					rb.setRegular_holiday(rs.getString("regular_holiday"));
+					rb.setRestaurant_id(rs.getInt("restaurant_id"));
+					rb.setUrl(rs.getString("url"));
+					searchResult.addResutaurantList(rb);
 				}
 			}
 		}catch (SQLException e) {
-			System.out.println("SQL");
+			e.printStackTrace();
 		}
-
 
 		return searchResult;
 
